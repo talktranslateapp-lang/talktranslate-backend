a454f585ab1c87e9bb85b9f616eb8ca324458c9c
talktranslateapp-lang <talktranslate.app@gmail.com>
2025-08-26 16:23:09 -0600

    TranslationCallAppApplication.java

---
+++ src/main/java/com/example/translationcallapp/TranslationCallAppApplication.java
@@ -0,0 +1,11 @@
+package com.example.translationcallapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslationCallAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TranslationCallAppApplication.class, args);
+    }
+}
