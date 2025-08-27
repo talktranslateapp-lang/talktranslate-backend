e5c55a259d2e2df125798c6b59ac42660a103294
talktranslateapp-lang <talktranslate.app@gmail.com>
2025-08-27 13:50:17 -0600

    CallController.java

--- src/main/java/com/example/translationcallapp/controller/CallController.java
+++ src/main/java/com/example/translationcallapp/controller/CallController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
