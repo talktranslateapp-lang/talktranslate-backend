b6548f4e7d5f8a1a3d44c9bd3019400394266e9f
talktranslateapp-lang <talktranslate.app@gmail.com>
2025-08-26 21:21:03 -0600

    CallController.java

--- src/main/java/com/example/translationcallapp/controller/CallController.java
+++ src/main/java/com/example/translationcallapp/controller/CallController.java
@@ -1,9 +1,12 @@
 package com.example.translationcallapp.controller;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*") // Allows all domains - you can restrict this to your v0 domain later
 public class CallController {
     
     @GetMapping("/health")
