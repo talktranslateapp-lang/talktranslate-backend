caf6cbdd8613b8e6bc932d838d32894df813ee26
talktranslateapp-lang <talktranslate.app@gmail.com>
2025-08-28 21:39:06 -0600

    SpringConfigurator.java

---
+++ SpringConfigurator.java
@@ -0,0 +1,25 @@
+package com.example.translationcallapp.websocket;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+import javax.websocket.server.ServerEndpointConfig;
+
+@Component
+public class SpringConfigurator extends ServerEndpointConfig.Configurator implements ApplicationContextAware {
+    
+    private static volatile BeanFactory context;
+    
+    @Override
+    public <T> T getEndpointInstance(Class<T> clazz) throws InstantiationException {
+        return context.getBean(clazz);
+    }
+    
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        SpringConfigurator.context = applicationContext;
+    }
+}
